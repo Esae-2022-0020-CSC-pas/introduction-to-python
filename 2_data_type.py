cfee94a6e84263cc75915648b78f90c69a4faee5
ESAE-2022-0020-CSC-PAS <seunrichard492@gmail.com>
2024-08-21 14:00:04 -0700

    creating a variable number

---
+++ 2_data_type.py
@@ -0,0 +1,17 @@
+num = 10
+num_type = type(num)
+print(num_type)
+print("num_type")
+
+
+name_1 = "zainab"
+name_2 = "blessing"
+name_3 = "muideen"
+name_4 ="richard"
+name_5 ="mr richard"
+
+data_type_name = type(name_4)
+print(data_type_name)
+
+sola_age = (40)
+print(sola_age)
