7e3e1d4a6bbe371571c2544f94f7175cad713f46
ESAE-2022-0020-CSC-PAS <seunrichard492@gmail.com>
2024-08-26 14:40:43 -0700

    pythonelseif.py

---
+++ pythonelseif.py
@@ -0,0 +1,17 @@
+age = int(input("how old are you "))
+if age  >= 18:
+    print("you are eligible to vote.")
+else:
+    print("you are not eligible to vote.")
+
+
+
+number = int(input("input your single number "))
+if number < 7:
+        print("print the number is negative")
+elif number > 7:
+    print("the number is positive")
+    print("welcome to negativity ")
+
+else:
+    print(" number not found use the link above to get your single number ")
